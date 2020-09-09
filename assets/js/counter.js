b611c06c401cedcf6fab83b6df4376e5b29a3951
Sayyid Mahmood <sayyidmahmood@outlook.com>
2020-09-09 13:32:22 +0530

    Final File uploaded

---
+++ assets/js/counter.js
@@ -0,0 +1,13 @@
+(function ($) {
+ "use strict";
+   
+/*----------------------------
+ Counter js active
+------------------------------ */
+    var count = $('.counter');
+    count.counterUp({
+		delay: 40,
+		time: 3000
+	});
+
+})(jQuery); 
